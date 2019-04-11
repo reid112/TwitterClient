e92f9148d5963b0f379002ab3b3bff091dd36beb
Reid <rreid112@gmail.com>
2019-04-10 20:19:28 -0600

    Binding list item

--- app/src/main/java/ca/rjreid/twitterclient/screens/list/ListAdapter.kt
+++ app/src/main/java/ca/rjreid/twitterclient/screens/list/ListAdapter.kt
@@ -1,22 +1,21 @@
 package ca.rjreid.twitterclient.screens.list
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ca.rjreid.twitterclient.R
+import ca.rjreid.twitterclient.databinding.ListItemTweetBinding
 import ca.rjreid.twitterclient.models.Tweet
 import ca.rjreid.twitterclient.screens.list.ListAdapter.TweetViewHolder
-import ca.rjreid.twitterclient.utils.image
-import kotlinx.android.synthetic.main.list_item_tweet.view.*
 import javax.inject.Inject
 
 class ListAdapter @Inject constructor() : ListAdapter<Tweet, TweetViewHolder>(DiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return TweetViewHolder(inflater.inflate(R.layout.list_item_tweet, parent, false))
+        return TweetViewHolder(DataBindingUtil.inflate(inflater, R.layout.list_item_tweet, parent, false))
     }
 
     override fun onBindViewHolder(holder: TweetViewHolder, position: Int) {
@@ -34,15 +33,10 @@ class ListAdapter @Inject constructor() : ListAdapter<Tweet, TweetViewHolder>(Di
         }
     }
 
-    class TweetViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class TweetViewHolder(val binding: ListItemTweetBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(tweet: Tweet) {
-            itemView.apply {
-                imageView.image(tweet.userImageUrl)
-                username.text = tweet.username
-                handle.text = tweet.handle
-                content.text = tweet.content
-            }
+            binding.tweet = tweet
         }
     }
 }
